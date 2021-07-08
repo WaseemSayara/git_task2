097adcd27d22718e86c36d85b51b9bd47b31bdd1
Waseem Sayara <waseem.ahmad1294@gmail.com>
2021-07-08 09:57:03 +0300

    fixed test function

--- tests.py
+++ tests.py
@@ -6,4 +6,4 @@ import pytest
                                            ("my Name is @12^^.", "my Name is @12^^."), (" ", " "), ("", ""),
                                            ("hi There 12@gmail \n", "hi There 12@gmail \n")])
 def test_lower(input1, input2):
-    assert main.make_lower_fun1(input1) == main.make_lower_fun1(input2)
+    assert main.make_lower_fun1(input1) == main.make_lower_fun2(input2)
