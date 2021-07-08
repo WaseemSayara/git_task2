1b4351880b43332a61aad0a7b71a7c4f432066e6
Waseem Sayara <waseem.ahmad1294@gmail.com>
2021-07-08 09:38:59 +0300

    added parametrization to tests file

--- tests.py
+++ tests.py
@@ -1,5 +1,9 @@
 import main
+import pytest
 
 
-def test_lower():
-    assert main.make_lower_fun1("WasEem SayAra Zz") == main.make_lower_fun1("WasEem SayAra Zz")
+@pytest.mark.parametrize("input1,input2", [("hello world", "hello world"), ("HellO WoRld", "HellO WoRld"),
+                                           ("my Name is @12^^.", "my Name is @12^^."), (" ", " "), ("", ""),
+                                           ("hi There 12@gmail \n", "hi There 12@gmail \n")])
+def test_lower(input1, input2):
+    assert main.make_lower_fun1(input1) == main.make_lower_fun1(input2)
